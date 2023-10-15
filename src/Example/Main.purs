ca9444c54209ea2011ae5d62cae7370d362a51cd
f <f@grybiena.com>
2023-10-15 14:08:40 +0200

    widget example of launching a UI as a shell program

--- src/Example/Main.purs
+++ src/Example/Main.purs
@@ -1,16 +1,11 @@
 module Example.Main where
-
+import Example.Widget (component)
 import Prelude
 
-import Data.String (trim)
-import Data.Tuple.Nested ((/\))
 import Effect (Effect)
 import Halogen.Aff as HA
-import Halogen.Shell (component)
-import Halogen.Shell.CommandLine (commandLine, runRepl, textInterpreter)
-import Halogen.Shell.Free (interpreter, terminal)
-import Halogen.Terminal.Free (cols, loadAddons, options, rows, write)
-import Halogen.Terminal.Free.Options (getCursorBlink, getFontFamily, setCursorBlink)
+
+
 import Halogen.VDom.Driver (runUI)
 
 
@@ -19,26 +14,5 @@ main :: Effect Unit
 main = do
   HA.runHalogenAff do
      body <- HA.awaitBody
-     let prompt = "$ "
-         repl s | trim s == "rows" = show <$> terminal rows 
-         repl s | trim s == "cols" = show <$> terminal cols
-         repl s | trim s == "blink" = show <$> terminal (options getCursorBlink)
-         repl s | trim s == "blinkon" = do
-            terminal $ options $ setCursorBlink true
-            pure ""
-         repl s | trim s == "blinkoff" = do
-            terminal $ options $ setCursorBlink false
-            pure ""
-         repl s | trim s == "fontFamily" = terminal $ options getFontFamily
-         repl s = pure s
-         shell =
-           { init: do
-               terminal do
-                 loadAddons true
-                 write prompt
-               interpreter (textInterpreter $ commandLine (runRepl repl))
-           , query: const (pure unit)
-           , shell: (prompt /\ "")
-           }
-     runUI component shell body
+     runUI component unit body
 
